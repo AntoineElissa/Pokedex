66d42793d7e7a246b714526ba9c2a0aec792491e
Antoine Elissagaray <antoine.elissagaray@orange.fr>
2023-05-30 20:20:24 +0200

    quete 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,42 @@
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
 function PokemonCard() {
 
+    const pokemon  = pokemonList[1];
+    let chaineHtml = "";
+
+    //Vérifie que imgSrc existe dans objet pokemon et qu'elle n'est pas vide
+    if("imgSrc" in pokemon && pokemon.imgSrc !== ""){
+        chaineHtml = <img src = {pokemon.imgSrc} alt={pokemon.name} />;
+    }
+    else {
+        chaineHtml = <p> ??? </p>;
+    }
+    
+   
     return( 
     
     <figure>
 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image"/>
-        
-        <figcaption>bulbasaur</figcaption>
+        {chaineHtml}
+        <figcaption>{pokemon.name}</figcaption>
     
     </figure>
 
     )
 }
 
+
 export default PokemonCard;
+
